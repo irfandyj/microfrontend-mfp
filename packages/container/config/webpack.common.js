d938a6172d5e5de47f17afee1873403ae3719980
Jip Irfandy <irfandyjip98@gmail.com>
2022-05-21 16:09:57 +0700

    fix: putting `plugins` in the wrong property

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -14,11 +14,11 @@ module.exports = {
           }
         }
       }
-    ],
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html'
-      })
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
 }
