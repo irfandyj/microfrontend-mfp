d72782244a90656b751f2ea60a1a6e98a1ac4457
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-01 12:28:13 +0700

    feat: add prefix for `container` css classes to prevent css name collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp.js'
 import Header from './components/Header.js'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
