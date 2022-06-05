6d13a38600b922d9265007337e9719917d1a7cb5
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-05 14:37:31 +0700

    feat: add `auth` components

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Switch, Route, Router } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 
+import SignIn from './components/Signin'
+import SignUp from './components/Signup'
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au'
 })
@@ -11,6 +14,8 @@ export default ({ history }) => {
     <StylesProvider generateClassName={generateClassName}>
       <Router history={history}>
         <Switch>
+          <Route path="/auth/signin" component={SignIn}></Route>
+          <Route path="/auth/signup" component={SignUp}></Route>
         </Switch>
       </Router>
     </StylesProvider>
