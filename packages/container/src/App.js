d95e76a469c57616550601a81ef20573aa7ba3f5
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-05 16:19:20 +0700

    refactor: update html for performance

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import MarketingApp from './components/MarketingApp.js'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './components/Header.js'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core'
+
+import MarketingApp from './components/MarketingApp.js'
+import AuthApp from './components/AuthApp.js'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -14,7 +16,10 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route path="/auth" component={AuthApp}></Route>
+            <Route path="/" component={MarketingApp}></Route>
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>
