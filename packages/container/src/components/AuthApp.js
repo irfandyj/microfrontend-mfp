80130ab122972bdb85f918fb2e91d7942d472617
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-05 15:44:50 +0700

    feat: add `AuthApp`

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,29 @@
+import { mount } from 'auth/AuthApp'
+import React, { useRef, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default () => {
+  const ref = useRef(null)
+  const history = useHistory()
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate (location) {
+        const { pathname: nextPathName } = location
+        const { pathname: currentPathName } = history.location
+
+        // To prevent infinite loop because of 2 way interaction
+        // 1. Browser History -> Memory History
+        // 2. Memory History -> Browser History
+        if (currentPathName !== nextPathName) {
+          console.log('Noticed navigation in Marketing')
+          history.push(nextPathName)
+        }
+      }
+    })
+
+    history.listen(onParentNavigate)
+  }, [])
+
+  return <div ref={ref} />
+}
