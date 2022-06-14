3f3af81bda474d9d53eb599af203f700972be98a
Jip Irfandy <irfandyjip98@gmail.com>
2022-06-14 22:36:48 +0700

    refactor: update app so vue app can run

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,47 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.js'
-// Used by react-router-dom
-// So you dont have to install
-import { createMemoryHistory, createBrowserHistory } from 'history'
+import { createApp } from 'vue'
+import Dashboard from './components/Dashboard.vue'
 
-const mount = (el, {
-  onNavigate,
-  onSignIn,
-  defaultHistory,
-  initialPath
-}) => {
-  const history = defaultHistory || createMemoryHistory({
-    initialEntries: [initialPath]
-  })
-
-  // Temporary fix, will update again later
-  if (onNavigate) {
-    history.listen(onNavigate)
-  }
-
-  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el)
-
-  return {
-    onParentNavigate (location) {
-      const { pathname: nextPathName } = location
-      const { pathname: currentPathName } = history.location
-
-      // To prevent infinite loop because of 2 way interaction
-      // 1. Browser History -> Memory History
-      // 2. Memory History -> Browser History
-      if (currentPathName !== nextPathName) {
-        console.log('Noticed container navigated')
-        history.push(nextPathName)
-      }
-    }
-  }
+const mount = (el) => {
+  const app = createApp(Dashboard)
+  app.mount(el)
 }
 
 if (process.env.NODE_ENV === 'development') {
   const el = document.getElementById('_dashboard-dev-root')
   if (el) {
-    mount(el, { defaultHistory: createBrowserHistory() })
+    mount(el)
   }
 }
 
